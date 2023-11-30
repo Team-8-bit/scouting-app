6be356c7325361b7acbc829c18ebef084a4f443b
nolan <nolan.s.locke@gmail.com>
2023-11-30 15:12:23 -0700

    display already scouted matches separately

--- app/src/main/java/org/team9432/scoutingapp/ui/screens/MatchSelectionScreen.kt
+++ app/src/main/java/org/team9432/scoutingapp/ui/screens/MatchSelectionScreen.kt
@@ -3,10 +3,9 @@ package org.team9432.scoutingapp.ui.screens
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ListItem
-import androidx.compose.material3.ListItemDefaults
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -19,17 +18,37 @@ import org.team9432.scoutingapp.io.config
 import org.team9432.scoutingapp.io.data.ScheduledMatch
 
 @Composable
-fun MatchSelectionScreen() {
-    LazyColumn(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
-        ScheduleFiles.getMatches(config.scoutID, config.eventID).forEach {
+fun MatchSelectionScreen(matches: List<ScheduledMatch>, scoutID: Int) {
+    val alreadyScoutedMatches = matches.filter { it.hasBeenScouted(it.teams[scoutID]!!.teamNumber) }
+    val toBeScoutedMatches = matches.filter { !alreadyScoutedMatches.contains(it) }
+
+    val scrollState = LazyListState(firstVisibleItemIndex = alreadyScoutedMatches.size + 1)
+    LazyColumn(Modifier.fillMaxSize(), state = scrollState, horizontalAlignment = Alignment.CenterHorizontally) {
+        alreadyScoutedMatches.forEach {
             item {
                 MatchDisplay(
                     data = it,
-                    hasBeenScouted = it.hasBeenScouted(it.teams[config.scoutID]!!.teamNumber),
+                    hasBeenScouted = true,
                     onClick = {
                         currentMatch = it.number
                         appScreen = Screen.MATCH_SCOUTING_SCREEN
-                    })
+                    }
+                )
+            }
+        }
+        item {
+            Divider()
+        }
+        toBeScoutedMatches.forEach {
+            item {
+                MatchDisplay(
+                    data = it,
+                    hasBeenScouted = false,
+                    onClick = {
+                        currentMatch = it.number
+                        appScreen = Screen.MATCH_SCOUTING_SCREEN
+                    }
+                )
             }
         }
     }
