2db0a8b2cf8e5b208254d4ac8619330e562d34a3
nolan <nolan.s.locke@gmail.com>
2023-12-02 17:27:01 -0700

    add default modifier to generated inputs

--- app/src/main/java/org/team9432/scoutingapp/ui/screens/MatchScoutingScreen.kt
+++ app/src/main/java/org/team9432/scoutingapp/ui/screens/MatchScoutingScreen.kt
@@ -34,6 +34,7 @@ fun MatchScoutingScreen(teamToScout: String, matchNumber: Int) {
             MatchScoutingDataInputs(
                 updateData = updateData,
                 initialData = matchData,
+                defaultModifier = { this.padding(5.dp) },
             )
         )
     }
@@ -61,20 +62,20 @@ private fun Notes(inputs: MatchScoutingDataInputs, setScreen: (Screen) -> Unit, 
     Column(Modifier.fillMaxSize()) {
         Row(Modifier.padding(5.dp).fillMaxHeight(0.6F).fillMaxWidth(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceEvenly) {
             Column(Modifier.fillMaxHeight().width(250.dp).padding(5.dp)) {
-                inputs.RobotRole(Modifier.width(250.dp).padding(5.dp))
-                inputs.SubstationPreference(Modifier.width(250.dp).padding(5.dp))
-                inputs.GamePiecePreference(Modifier.width(250.dp).padding(5.dp), title = "GP Preference")
+                inputs.RobotRole(Modifier.width(250.dp))
+                inputs.SubstationPreference(Modifier.width(250.dp))
+                inputs.GamePiecePreference(Modifier.width(250.dp), title = "GP Preference")
             }
             Column(Modifier.fillMaxHeight().width(250.dp).padding(5.dp)) {
-                inputs.DefenceQuality(Modifier.width(250.dp).padding(5.dp))
-                inputs.DrivingQuality(Modifier.width(250.dp).padding(5.dp))
-                inputs.WouldPick(Modifier.width(250.dp).padding(5.dp))
+                inputs.DefenceQuality(Modifier.width(250.dp))
+                inputs.DrivingQuality(Modifier.width(250.dp))
+                inputs.WouldPick(Modifier.width(250.dp))
             }
             BlankInput(Modifier.fillMaxHeight().fillMaxWidth(0.5F).padding(5.dp))
             SubmitButton(Modifier.fillMaxSize().padding(5.dp), onPressed = onSave)
         }
         Row(Modifier.padding(5.dp).fillMaxHeight().fillMaxWidth(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceEvenly) {
-            inputs.Notes(Modifier.fillMaxHeight().fillMaxWidth(0.85F).padding(5.dp))
+            inputs.Notes(Modifier.fillMaxHeight().fillMaxWidth(0.85F))
             PageChanger(Modifier.fillMaxSize().padding(5.dp), onNext = {}, onBack = { setScreen(Screen.TELEOP) }, nextEnabled = false)
         }
     }
@@ -84,24 +85,24 @@ private fun Notes(inputs: MatchScoutingDataInputs, setScreen: (Screen) -> Unit, 
 private fun Teleop(inputs: MatchScoutingDataInputs, setScreen: (Screen) -> Unit) {
     Column(Modifier.fillMaxSize()) {
         Row(Modifier.padding(5.dp).fillMaxHeight(0.33F).fillMaxWidth(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceEvenly) {
-            inputs.TeleConesTop(Modifier.fillMaxHeight().width(175.dp).padding(5.dp))
-            inputs.TeleConesMid(Modifier.fillMaxHeight().width(175.dp).padding(5.dp))
-            inputs.TeleConesLow(Modifier.fillMaxHeight().width(175.dp).padding(5.dp))
-            inputs.Defended(Modifier.fillMaxHeight().fillMaxWidth(0.5F).padding(5.dp))
-            inputs.Disabled(Modifier.fillMaxHeight().fillMaxWidth().padding(5.dp))
+            inputs.TeleConesTop(Modifier.fillMaxHeight().width(175.dp))
+            inputs.TeleConesMid(Modifier.fillMaxHeight().width(175.dp))
+            inputs.TeleConesLow(Modifier.fillMaxHeight().width(175.dp))
+            inputs.Defended(Modifier.fillMaxHeight().fillMaxWidth(0.5F))
+            inputs.Disabled(Modifier.fillMaxHeight().fillMaxWidth())
         }
         Row(Modifier.padding(5.dp).fillMaxHeight(0.5F).fillMaxWidth(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceEvenly) {
-            inputs.TeleCubesTop(Modifier.fillMaxHeight().width(175.dp).padding(5.dp))
-            inputs.TeleCubesMid(Modifier.fillMaxHeight().width(175.dp).padding(5.dp))
-            inputs.TeleCubesLow(Modifier.fillMaxHeight().width(175.dp).padding(5.dp))
-            inputs.Penalties(Modifier.fillMaxHeight().width(175.dp).padding(5.dp))
-            inputs.TeleEngage(Modifier.fillMaxHeight().fillMaxWidth().padding(5.dp))
+            inputs.TeleCubesTop(Modifier.fillMaxHeight().width(175.dp))
+            inputs.TeleCubesMid(Modifier.fillMaxHeight().width(175.dp))
+            inputs.TeleCubesLow(Modifier.fillMaxHeight().width(175.dp))
+            inputs.Penalties(Modifier.fillMaxHeight().width(175.dp))
+            inputs.TeleEngage(Modifier.fillMaxHeight().fillMaxWidth())
         }
         Row(Modifier.padding(5.dp).fillMaxHeight().fillMaxWidth(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceEvenly) {
-            inputs.ConesDropped(Modifier.fillMaxHeight().width(175.dp).padding(5.dp))
-            inputs.CubesDropped(Modifier.fillMaxHeight().width(175.dp).padding(5.dp))
-            inputs.ConesMissed(Modifier.fillMaxHeight().width(175.dp).padding(5.dp))
-            inputs.CubesMissed(Modifier.fillMaxHeight().width(175.dp).padding(5.dp))
+            inputs.ConesDropped(Modifier.fillMaxHeight().width(175.dp))
+            inputs.CubesDropped(Modifier.fillMaxHeight().width(175.dp))
+            inputs.ConesMissed(Modifier.fillMaxHeight().width(175.dp))
+            inputs.CubesMissed(Modifier.fillMaxHeight().width(175.dp))
             PageChanger(Modifier.fillMaxSize().padding(5.dp), onNext = { setScreen(Screen.NOTES) }, onBack = { setScreen(Screen.AUTO) })
         }
     }
@@ -111,17 +112,17 @@ private fun Teleop(inputs: MatchScoutingDataInputs, setScreen: (Screen) -> Unit)
 private fun Auto(inputs: MatchScoutingDataInputs, setScreen: (Screen) -> Unit) {
     Column(Modifier.fillMaxSize()) {
         Row(Modifier.padding(5.dp).fillMaxHeight(0.5F).fillMaxWidth(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceEvenly) {
-            inputs.AutoMobility(Modifier.fillMaxHeight().width(100.dp).padding(5.dp))
-            inputs.AutoConesTop(Modifier.fillMaxHeight().width(200.dp).padding(5.dp))
-            inputs.AutoConesMid(Modifier.fillMaxHeight().width(200.dp).padding(5.dp))
-            inputs.AutoConesLow(Modifier.fillMaxHeight().width(200.dp).padding(5.dp))
+            inputs.AutoMobility(Modifier.fillMaxHeight().width(100.dp))
+            inputs.AutoConesTop(Modifier.fillMaxHeight().width(200.dp))
+            inputs.AutoConesMid(Modifier.fillMaxHeight().width(200.dp))
+            inputs.AutoConesLow(Modifier.fillMaxHeight().width(200.dp))
             BlankInput(Modifier.fillMaxHeight().fillMaxWidth().padding(5.dp))
         }
         Row(Modifier.padding(5.dp).fillMaxHeight().fillMaxWidth(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceEvenly) {
-            inputs.AutoEngage(Modifier.fillMaxHeight().width(150.dp).padding(5.dp))
-            inputs.AutoCubesTop(Modifier.fillMaxHeight().width(200.dp).padding(5.dp))
-            inputs.AutoCubesMid(Modifier.fillMaxHeight().width(200.dp).padding(5.dp))
-            inputs.AutoCubesLow(Modifier.fillMaxHeight().width(200.dp).padding(5.dp))
+            inputs.AutoEngage(Modifier.fillMaxHeight().width(150.dp))
+            inputs.AutoCubesTop(Modifier.fillMaxHeight().width(200.dp))
+            inputs.AutoCubesMid(Modifier.fillMaxHeight().width(200.dp))
+            inputs.AutoCubesLow(Modifier.fillMaxHeight().width(200.dp))
             PageChanger(Modifier.fillMaxSize().padding(5.dp), onNext = { setScreen(Screen.TELEOP) }, onBack = { setScreen(Screen.PRE_MATCH) })
         }
     }
@@ -129,10 +130,10 @@ private fun Auto(inputs: MatchScoutingDataInputs, setScreen: (Screen) -> Unit) {
 
 @Composable
 private fun PreMatch(inputs: MatchScoutingDataInputs, setScreen: (Screen) -> Unit) {
-    Column(Modifier.fillMaxSize()) {
-        inputs.MatchNumber(Modifier.fillMaxWidth().padding(5.dp))
-        inputs.ScoutID(Modifier.fillMaxWidth().padding(5.dp), title = "Scout ID")
-        inputs.TeamNumber(Modifier.fillMaxWidth().padding(5.dp), title = "Team to Scout")
+    Column(Modifier.fillMaxSize().padding(5.dp)) {
+        inputs.MatchNumber(Modifier.fillMaxWidth())
+        inputs.ScoutID(Modifier.fillMaxWidth(), title = "Scout ID")
+        inputs.TeamNumber(Modifier.fillMaxWidth(), title = "Team to Scout")
 
         Row {
             BlankInput(Modifier.fillMaxHeight().fillMaxWidth(0.5F).padding(5.dp))
