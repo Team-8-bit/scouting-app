1bf0877e99fa0fb9491c2f9729e9edae347c57d1
nolan <nolan.s.locke@gmail.com>
2024-01-16 19:04:36 -0700

    make qr codes monochrome

--- app/src/main/java/org/team9432/scoutingapp/io/QRCodes.kt
+++ app/src/main/java/org/team9432/scoutingapp/io/QRCodes.kt
@@ -10,13 +10,6 @@ import qrcode.QRCode
 object QRCodes {
     fun fromString(string: String): ImageBitmap {
         val test = QRCode.ofSquares()
-        if (config.darkMode) {
-            test.withGradientColor(0xFF356943.toInt(), 0xFF675A92.toInt())
-            test.withBackgroundColor(0x000000)
-        } else {
-            test.withGradientColor(0xFF22462C.toInt(), 0xFF493D70.toInt())
-            test.withBackgroundColor(0x000000)
-        }
 
         test.withSize(10)
         test.withInnerSpacing(0)
