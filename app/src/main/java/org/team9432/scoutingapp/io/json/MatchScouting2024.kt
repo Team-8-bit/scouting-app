dee697fa39bc99c24665616e9edf10068df1c943
nolan <nolan.s.locke@gmail.com>
2024-02-09 21:55:26 -0700

    change driving and defence scales

--- app/src/main/java/org/team9432/scoutingapp/io/json/MatchScouting2024.kt
+++ app/src/main/java/org/team9432/scoutingapp/io/json/MatchScouting2024.kt
@@ -44,8 +44,8 @@ data class MatchScoutingData(
 
     @NumberInputField val scoredTrap: Int = 0,
     @CycleInputField("No Attempt", "Parked", "Single", "Harmony x1", "Harmony x2") val endgame: String = "N/A",
-    @CycleInputField("Excellent", "Acceptable", "Subpar", "Abysmal") val defenceQuality: String = "N/A",
-    @CycleInputField("good", "ok", "🤨", "no") val drivingQuality: String = "N/A",
+    @NumberInputField(max = 4, min = 1) val defenceQuality: Int = 1,
+    @NumberInputField(max = 4, min = 1) val drivingQuality: Int = 1,
     @NumberInputField val penalties: Int = 0,
     @SwitchInputField val disabled: Boolean = false,
     @TextInputField val notes: String = "",
