096451d22ce16502ba4eee47ad745a57c8074817
nolan <nolan.s.locke@gmail.com>
2024-03-20 12:17:02 -0700

    add team reminder during on scouting screens

--- app/src/main/java/org/team9432/scoutingapp/ui/screens/scoutingscreens/MatchScoutingScreen.kt
+++ app/src/main/java/org/team9432/scoutingapp/ui/screens/scoutingscreens/MatchScoutingScreen.kt
@@ -1,9 +1,19 @@
 package org.team9432.scoutingapp.ui.screens.scoutingscreens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import org.team9432.scoutingapp.io.config
 import org.team9432.scoutingapp.ui.screens.scoutingscreens.matchscouting.MatchScouting
 import org.team9432.scoutingapp.ui.screens.scoutingscreens.superscouting.Superscouting
@@ -11,10 +21,15 @@ import org.team9432.scoutingapp.ui.screens.scoutingscreens.superscouting.Supersc
 
 @Composable
 fun MatchScoutingScreen(teamToScout: String, matchNumber: String) {
-    if (config.isSuperscout) {
-        Superscouting(teamToScout, matchNumber)
-    } else {
-        MatchScouting(teamToScout, matchNumber)
+    Column {
+        Box(Modifier.fillMaxWidth().height(20.dp).background(MaterialTheme.colorScheme.surface), contentAlignment = Alignment.Center) {
+            Text("Team to Scout: $teamToScout", textAlign = TextAlign.Center)
+        }
+        if (config.isSuperscout) {
+            Superscouting(teamToScout, matchNumber)
+        } else {
+            MatchScouting(teamToScout, matchNumber)
+        }
     }
 }
 
